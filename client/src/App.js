39f5cbd2486f73b16d13bc20039e6ea32af47cf7
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-12 18:59:53 -0800

    client/app.js updated with imports for CssBaseline, ThemeProvider, createTheme, themeSettings, useSelector, useMemo, BrowserRouter, Routes, Route, client/src/scenes/dashbaord folders created with index.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<Navigate to="/dashboard" replace /> } />
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+            </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
