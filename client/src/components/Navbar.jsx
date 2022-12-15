3eec90ee56d3908f02ba442fd945b9973568008b
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-14 17:08:20 -0800

    Navbar right-side updated with User icon, name and occupation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
     LightModeOutlined, 
     DarkModeOutlined, 
@@ -16,15 +16,24 @@ import {
     useTheme, 
     Toolbar, 
     IconButton, 
-    InputBase 
+    InputBase,
+    Button,
+    Box,
+    Typography 
 } from '@mui/material';
 
 const Navbar = ({
+    user,
     isSidebarOpen,
     setIsSidebarOpen,
 }) => {
     const dispatch = useDispatch();
     const theme = useTheme();
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const isOpen = Boolean(anchorEl);
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
 
     return <AppBar
         sx={{
@@ -63,6 +72,41 @@ const Navbar = ({
                 <IconButton>
                     <SettingsOutlined sx={{ fontsize: '25px' }} />
                 </IconButton>
+
+                <FlexBetween>
+                    <Button 
+                        onClick={handleClick} 
+                        sx={{ 
+                            display: 'flex', 
+                            justifyContent: 'space-between', 
+                            alignItems: 'center', 
+                            textTransform: 'none', 
+                            gap: '1rem'
+                            }}>
+                        <Box 
+                            component='img'
+                            alt='profile'
+                            src={profileImage}
+                            height='32px'
+                            width='32px'
+                            borderRadius='50%'
+                            sx={{ objectFit: 'cover' }}
+                        />
+                        <Box textAlign='left'>
+                            <Typography 
+                                fontWeight='bold' 
+                                fontSize='0.85rem' 
+                                sx={{ color: theme.palette.secondary[100]}}>
+                                    {user.name}
+                            </Typography>
+                            <Typography 
+                                fontSize='0.75rem' 
+                                sx={{ color: theme.palette.secondary[200]}}>
+                                    {user.occupation}
+                            </Typography>
+                        </Box>        
+                    </Button>
+                </FlexBetween>
             </FlexBetween>
         </Toolbar>
     </AppBar>
