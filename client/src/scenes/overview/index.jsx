cb85fc59c273cd6e54af4f0de6d86818e99fcc8f
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-18 16:06:02 -0800

    Overview page updated with sales endpoint to visualize total sales and total units sold by month using nivo line chart npm installed

---
+++ client/src/scenes/overview/index.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import { FormControl, MenuItem, InputLabel, Box, Select } from '@mui/material';
+import Header from 'components/Header';
+import OverviewChart from 'components/OverviewChart';
+
+const Overview = () => {
+    const [view, setView] = useState("units");
+
+  return (
+    <Box m='1.5rem 2.5rem'>
+        <Header title='OVERVIEW' subtitle='Visualization of general revenue and profit flow!' />
+        <Box
+            height='75vh'
+        >
+            <FormControl
+                sx={{ mt: '1rem' }}
+            >
+                <InputLabel>
+                    View
+                </InputLabel>
+                <Select
+                    value={view}
+                    label="View"
+                    onChange={(e) => setView(e.target.value)}
+                >
+                    <MenuItem
+                        value='sales'
+                    >
+                        Sales
+                    </MenuItem>
+                    <MenuItem
+                        value='units'
+                    >
+                        Units
+                    </MenuItem>
+                </Select>
+            </FormControl>
+            <OverviewChart
+                view={view}
+            >
+            </OverviewChart>
+        </Box>
+    </Box>
+  );
+};
+
+export default Overview
--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ import User from './models/User.js';
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js';
+import OverallStat from "./models/OverallStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
 
 // SERVER Configurations
 dotenv.config();
@@ -49,6 +50,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server PORT running at: ${PORT}`));
 
     // ADD USER DATA ONLY ONE TIME!
+    OverallStat.insertMany(dataOverallStat);
     // Product.insertMany( dataProduct);
     // ProductStat.insertMany( dataProductStat);
     // Transaction.insertMany(dataTransaction);
