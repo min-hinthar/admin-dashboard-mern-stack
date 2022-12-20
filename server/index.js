a47b6eef072c70895cfe3062a2365cae8db41b70
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-20 15:26:19 -0800

    getUserPerformanceQuery endpoint created for management panel, mongoose aggregrate  and  used to map and filter sales by userId from database, scenes/performance created to render on page reusing custom components

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
+import AffiliateStat from "./models/AffiliateStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from './data/index.js';
 
 // SERVER Configurations
 dotenv.config();
@@ -50,6 +51,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server PORT running at: ${PORT}`));
 
     // ADD USER DATA ONLY ONE TIME!
+    // AffiliateStat.insertMany(dataAffiliateStat);
     // OverallStat.insertMany(dataOverallStat);
     // Product.insertMany( dataProduct);
     // ProductStat.insertMany( dataProductStat);
